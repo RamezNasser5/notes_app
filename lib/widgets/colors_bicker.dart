ebed46653c796d7bd8897ab6e081d3339749920a
RamezNasser5 <rameznasser55@gmail.com>
2023-12-03 14:29:10 +0200

    colors picker part 2

--- lib/widgets/colors_bicker.dart
+++ lib/widgets/colors_bicker.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isPicked, required this.color});
+  final bool isPicked;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 32,
-      backgroundColor: Colors.blueAccent,
-    );
+    return isPicked
+        ? Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: CircleAvatar(
+              radius: 32,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: color,
+              ),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: CircleAvatar(
+              radius: 32,
+              backgroundColor: color,
+            ),
+          );
   }
 }
 
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xFFe8e9f3),
+    Color(0xFFdbdce1),
+    Color(0xFFcecece),
+    Color(0xFFbababb),
+    Color(0xFFa6a6a8),
+    Color(0xFF67666f),
+    Color(0xFF474652),
+    Color(0xFF272635),
+    Color(0xFF3b3a47),
+    Color(0xFF4d4c58),
+  ];
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -26,7 +61,16 @@ class ColorListView extends StatelessWidget {
           itemCount: 10,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return const ColorItem();
+            return GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isPicked: currentIndex == index,
+              ),
+            );
           },
         ),
       ),
